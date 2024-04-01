443e57ce9491d898b538ac1d5d6496faf4d4dc3e
dAHTvind888 <159574540+dAHTvind888@users.noreply.github.com>
2024-04-01 08:09:59 -0400

    exercise_2 try2

--- src/exercises.cpp
+++ src/exercises.cpp
@@ -18,7 +18,7 @@ void exercise_1(string s1) {
 
 void exercise_2(string s1) {
   // TODO: YOUR CODE HERE
-  string word;
+  string word = "";
   for(int i = 0; i <= s1.size(); i++){
     if(s1[i] == ' '){
       cout << "[" << word << "]" << endl;
