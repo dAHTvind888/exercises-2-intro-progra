591171acc4aaf6caef984fc11cf71cda832d1f10
dAHTvind888 <159574540+dAHTvind888@users.noreply.github.com>
2024-04-01 10:07:08 -0400

    exercise_3 try2

--- src/exercises.cpp
+++ src/exercises.cpp
@@ -42,7 +42,7 @@ void exercise_2(string s1) {
 
 void exercise_3(string s1) {
   // TODO: YOUR CODE HERE
-  for(int i = 0; i < 10; i += 2){
+  for(int i = 0; i < 20; i += 2){
     if(s1[i] == '1'){
       cout << "Om-nom-nom :P" << endl;
     }
