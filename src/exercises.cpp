9c17d5ae377d1651bc21715b4841cbe752186d6b
dAHTvind888 <159574540+dAHTvind888@users.noreply.github.com>
2024-04-01 09:59:40 -0400

    exercise_3 try1

--- src/exercises.cpp
+++ src/exercises.cpp
@@ -45,7 +45,6 @@ void exercise_3(string s1) {
   for(int i = 0; i < 10; i++){
     if(s1[i] == '1'){
       cout << "Om-nom-nom :P" << endl;
-      i++;
     }
     else if(s1[i] == ' ' && s1[i + 1] == '1'){
       cout << "Om-nom-nom :P" << endl;
