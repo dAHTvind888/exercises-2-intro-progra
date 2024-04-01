164d69e8c9358ccc362d090a912e7a11e8d5489b
dAHTvind888 <159574540+dAHTvind888@users.noreply.github.com>
2024-04-01 09:48:44 -0400

    exercise_3 try7

--- src/exercises.cpp
+++ src/exercises.cpp
@@ -49,7 +49,7 @@ void exercise_3(string s1) {
     else if(s1[i] == ' ' && s1[i + 1] == '1'){
       cout << "Om-nom-nom :P" << endl;
     }
-    else if(s1[i] == ' ' && s1[i + 1] == '1'){
+    else if(s1[i] == ' ' && s1[i + 1] == '0'){
       cout << "No cake :(" << endl;
       break;
     }
